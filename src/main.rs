adbdf944a4355d3f98b898b4a3821dd458fd11fd
Honza Pokorny <honza@redhat.com>
2020-11-06 12:47:12 -0400

    meh

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,43 @@
-use std::io::{stdout, Read, Write};
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::sync::Arc;
+use url::Url;
 
-use rustls;
-use webpki;
-use webpki_roots;
-
-use rustls::Session;
+use native_tls::TlsConnector;
 
 // gemini://drewdevault.com/2020/09/27/Gemini-and-Hugo.gmi
 // gemini://gemini.circumlunar.space/software/
 
-pub struct FakeVerifier {}
+fn gemini_request(url: Url) {
+    let host = url.host_str().unwrap();
+    let mut builder = TlsConnector::builder();
 
-impl FakeVerifier {
-    pub fn new() -> FakeVerifier {
-        FakeVerifier {}
-    }
-}
+    // Self-signed certificates are considered invalid, but they are quite
+    // common for gemini servers. Therefore, we accept invalid certs,
+    // but check for expiration later
+    builder.danger_accept_invalid_certs(true);
 
-impl rustls::ServerCertVerifier for FakeVerifier {
-    fn verify_server_cert(
-        &self,
-        roots: &rustls::RootCertStore,
-        presented_certs: &[rustls::Certificate],
-        dns_name: webpki::DNSNameRef,
-        ocsp_response: &[u8],
-    ) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
-        println!("verify cert");
-        Ok(rustls::ServerCertVerified::assertion())
-    }
+    // let connector = TlsConnector::new().unwrap();
+    let connector = builder.build().unwrap();
+
+    let connect_str = host.to_owned() + ":1965";
+    let stream = TcpStream::connect(connect_str).unwrap();
+    let mut stream = connector.connect(host, stream).unwrap();
+
+    // let certificate = stream.peer_certificate().unwrap().unwrap();
+    // let sig = certificate.to_der().unwrap();
+    // println!("{:?}", sig);
+
+    let request = url.into_string() + "\r\n";
+    stream.write_all(request.as_bytes()).unwrap();
+
+    let mut res = vec![];
+    stream.read_to_end(&mut res).unwrap();
+    println!("-> {}", String::from_utf8_lossy(&res));
 }
 
 fn main() {
-    let mut config = rustls::ClientConfig::new();
-    config
-        .root_store
-        .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
-    config
-        .dangerous()
-        .set_certificate_verifier(Arc::new(FakeVerifier::new()));
-    let dns_name = webpki::DNSNameRef::try_from_ascii_str("gus.guru").unwrap();
-    let mut sess = rustls::ClientSession::new(&Arc::new(config), dns_name);
-    let mut sock = TcpStream::connect("gus.guru:1965").unwrap();
-    let mut tls = rustls::Stream::new(&mut sess, &mut sock);
-    tls.write("gus.guru\r\n".as_bytes()).unwrap();
-    let ciphersuite = tls.sess.get_negotiated_ciphersuite().unwrap();
-    writeln!(
-        &mut std::io::stderr(),
-        "Current ciphersuite: {:?}",
-        ciphersuite.suite
-    )
-    .unwrap();
-    let mut plaintext = Vec::new();
-    tls.read_to_end(&mut plaintext).unwrap();
-    stdout().write_all(&plaintext).unwrap();
+    // let url = Url::parse("gemini://drewdevault.com/2020/09/27/Gemini-and-Hugo.gmi").unwrap();
+    let url = Url::parse("gemini://drewdevault.com").unwrap();
+    // let url = Url::parse("gemini://tanelorn.city/").unwrap();
+    gemini_request(url);
 }
