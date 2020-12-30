ca048ae384245ee2b653dcd168569eb9bcdee16b
Honza Pokorny <honza@redhat.com>
2020-12-30 19:56:25 -0400

    Add license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+//  gemini client in rust
+//  Copyright (C) 2020 Honza Pokorny <me@honza.ca>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 use openssl::hash::MessageDigest;
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use openssl::x509::X509;
